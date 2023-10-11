6951a72b6f5a6a6359ed770fed5af26cc33ddd4d
Nilansh Audichya <109611014+Nilansh1210@users.noreply.github.com>
2023-10-11 12:05:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="header">
+            <img src="" className="nav-logo" alt="Logo"/>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
